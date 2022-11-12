6acb76eb2cba87fc6a8fd18210ce020ccd9ec66d
Shantanu <97823352+ShawnMaz@users.noreply.github.com>
2022-11-11 21:36:49 -0500

    added the render test for the gallery component:

---
+++ src/components/Gallery/__tests__/index.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {render, cleanup, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Gallery from '..';
+
+const portrait = {name:'portraits', description:'Portraits of people in my life'};
+
+afterEach(cleanup);
+
+describe('Gallery is rendering', () => {
+    it('renders', () => {
+        render(
+            <Gallery currentCategory={portrait} />
+        );
+    });
+});
