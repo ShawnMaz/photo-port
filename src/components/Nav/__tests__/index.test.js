11381014026b99af106820f50c85468f8eb9acc2
Shantanu <97823352+ShawnMaz@users.noreply.github.com>
2022-11-11 21:29:30 -0500

    updated the test for the Nav component

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,31 +3,54 @@ import {render, cleanup, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+    {name:'portraits', description:'Portraits of people in my life'}
+];
+
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
     // baseline test
     it('renders', () => {
-        render(<Nav />);
+        render(<Nav 
+            categories = {categories}
+            setCurrentCategory = {mockSetCurrentCategory}
+            currentCategory = {mockCurrentCategory}
+        />);
     });
 
     // snapshot test
     it('matches snapshot', () => {
-        const {asFragment} = render(<Nav />);
+        const {asFragment} = render(<Nav 
+            categories = {categories}
+            setCurrentCategory = {mockSetCurrentCategory}
+            currentCategory = {mockCurrentCategory}
+        />);
         expect(asFragment()).toMatchSnapshot();
     });
 });
 
 describe('emoji is visible', () => {
     it('inserts emoji into h2', () => {
-        render(<Nav />);
+        render(<Nav 
+            categories = {categories}
+            setCurrentCategory = {mockSetCurrentCategory}
+            currentCategory = {mockCurrentCategory}
+        />);
         expect(screen.getByLabelText('camera')).toHaveTextContent('📸');
     });
 });
 
 describe('links are visible', () => {
     it('inserts test into links', () => {
-        render(<Nav />);
+        render(<Nav 
+            categories = {categories}
+            setCurrentCategory = {mockSetCurrentCategory}
+            currentCategory = {mockCurrentCategory}
+        />);
         expect(screen.getByTestId('link')).toHaveTextContent('Oh Snap!');
         expect(screen.getByTestId('about')).toHaveTextContent('About me');
     });
